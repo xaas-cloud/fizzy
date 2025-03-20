743597d059fea30013788e12680421fe9754b8ea
Mike Dalessio <mike@37signals.com>
2025-03-20 10:58:44 -0400

    Quick fix to restore the 404 page

--- config/initializers/tenanting.rb
+++ config/initializers/tenanting.rb
@@ -3,7 +3,7 @@ Rails.application.config.after_initialize do
     # in production, we're using a two-level subdomain like "tenant.fizzy.37signals.com", but in
     # development and beta it's only a single-level subdomain.
     tld_length = request.domain == "37signals.com" ? 2 : 1
-    request.subdomain(tld_length)
+    request.subdomain(tld_length).presence || "no-such-tenant-exists" # this is a quick fix for now
   end
 end
 
