c6c9e0d5fc0bf404f97bf65c1486530eae3e5cec
Jason Zimdars <jz@37signals.com>
2025-05-28 15:26:41 -0500

    Rework system events
    - Restyle system events so they don't look like comments and aren't attributed to the system user
    - Ensure the event creator is included in the description so it's clear who did it.
    - Walk back system comment replacement. Yes, it potentially avoids embarassing people when they change their mind, it's also losing data. If two intentional assigments happen in proximity, we've been only showing the latest one,.

--- app/models/card/eventable/system_commenter.rb
+++ app/models/card/eventable/system_commenter.rb
@@ -8,46 +8,22 @@ class Card::Eventable::SystemCommenter
   def comment
     return unless comment_body.present?
 
-    if comment = find_replaceable_system_comment
-      comment.update! body: comment_body
-    else
-      card.comments.create! creator: User.system, body: comment_body, created_at: event.created_at
-    end
+    card.comments.create! creator: User.system, body: comment_body, created_at: event.created_at
   end
 
   private
-    RELATED_EVENTS = [
-      %w[ card_assigned card_unassigned ]
-    ]
-
     def comment_body
       case event.action
       when "card_assigned"
-        "Assigned to #{event.assignees.pluck(:name).to_sentence}."
+        "#{event.creator.name} assigned this to #{event.assignees.pluck(:name).to_sentence}."
       when "card_unassigned"
-        "Unassigned from #{event.assignees.pluck(:name).to_sentence}."
+        "#{event.creator.name} unassigned from #{event.assignees.pluck(:name).to_sentence}."
       when "card_staged"
         "#{event.creator.name} moved this to '#{event.stage_name}'."
       when "card_closed"
-        "Closed by #{ event.creator.name }"
+        "Closed as “#{ card.closure.reason }” by #{ event.creator.name }"
       when "card_title_changed"
-        "#{event.creator.name} changed title from '#{event.particulars.dig('particulars', 'old_title')}' to '#{event.particulars.dig('particulars', 'new_title')}'."
+        "#{event.creator.name} changed the title from '#{event.particulars.dig('particulars', 'old_title')}' to '#{event.particulars.dig('particulars', 'new_title')}'."
       end
-    end
-
-    def find_replaceable_system_comment
-      previous_comment, previous_event = card.comments.last, card.events[-2]
-      if previous_comment&.creator&.system? && replaceable_event?(previous_event, event)
-        previous_comment
-      end
-    end
-
-    def replaceable_event?(candidate_event, replacement_event)
-      candidate_event && replacement_event &&
-        (candidate_event.action == replacement_event.action || related_events?(candidate_event, replacement_event))
-    end
-
-    def related_events?(candidate_event, replacement_event)
-      RELATED_EVENTS.find { |related_actions| related_actions.include?(candidate_event.action) && related_actions.include?(replacement_event.action) }
     end
 end
