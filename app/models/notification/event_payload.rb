6e9de6a017de172f3398e7a0dda8637af9109a38
Stanko Krtalić <stanko@stanko.io>
2026-02-27 19:59:18 +0100

    Revert "Push event payload body updates"

--- app/models/notification/event_payload.rb
+++ app/models/notification/event_payload.rb
@@ -22,32 +22,8 @@ class Notification::EventPayload < Notification::DefaultPayload
       card.closure ? "Moved to Done by #{event.creator.name}" : "Closed by #{event.creator.name}"
     when "card_reopened"
       "Reopened by #{event.creator.name}"
-    when "card_triaged"
-      if column_name.present?
-        "Moved to #{column_name} by #{event.creator.name}"
-      else
-        "Moved by #{event.creator.name}"
-      end
-    when "card_sent_back_to_triage"
-      "Moved back to Maybe? by #{event.creator.name}"
-    when "card_board_changed", "card_collection_changed"
-      if new_location_name.present?
-        "Moved to #{new_location_name} by #{event.creator.name}"
-      else
-        "Moved by #{event.creator.name}"
-      end
-    when "card_title_changed"
-      if new_title.present?
-        "Renamed to #{new_title} by #{event.creator.name}"
-      else
-        "Renamed by #{event.creator.name}"
-      end
-    when "card_postponed"
-      "Moved to Not Now by #{event.creator.name}"
-    when "card_auto_postponed"
-      "Moved to Not Now due to inactivity"
     else
-      "Updated by #{event.creator.name}"
+      event.creator.name
     end
   end
 
@@ -79,19 +55,6 @@ class Notification::EventPayload < Notification::DefaultPayload
 
     def card_title
       card.title.presence || "Card #{card.number}"
-    end
-
-    def column_name
-      event.particulars.dig("particulars", "column")
-    end
-
-    def new_location_name
-      event.particulars.dig("particulars", "new_board") ||
-        event.particulars.dig("particulars", "new_collection")
-    end
-
-    def new_title
-      event.particulars.dig("particulars", "new_title")
     end
 
     def card_url_with_comment_anchor(comment)
